a016a63aec631b4d6f526832e69af0288cb9b0f5
Adlucio Guimarães <adluciodev@gmail.com>
2023-12-16 21:42:49 -0400

    first commit

---
+++ src/components/SideMenu/SideMenu.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './SideMenu.css';
+
+export const SideMenu = () =>{
+    return (
+        <aside class = 'sidemenu'>
+            <div class = 'sidemenu-button'>
+                <span>+</span>
+                 Novo Chat
+            </div>
+        </aside>
+    )
+}
